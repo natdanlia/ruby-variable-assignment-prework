bf071b87605f02860cf8c90d229fb6fa18e533f9
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-07-31 11:58:40 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,11 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+
+
+name = "Joe"
+ 
+def greeting(name)
+   puts "Hello, #{name}"
+   binding.pry
+end
+greeting(name)
